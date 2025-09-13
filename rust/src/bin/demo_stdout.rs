eb222f63e7f92025f3bc36732f22cba67616c738
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-09-13 21:56:41 +0200

    rust: rename test_stdout to demo_stdout

--- rust/src/bin/demo_stdout.rs
+++ rust/src/bin/demo_stdout.rs
@@ -1,14 +1,14 @@
 use emtrace::{C_STYLE_FORMAT, Out, magic_address_bytes, trace, traceln};
 use std::io::Write;
 
-fn main() -> () {
+fn main() {
     std::io::stdout()
         .lock()
         .write_all(&magic_address_bytes())
         .unwrap();
     trace!("{}", i32: 0);
-    // trace!("Yeeha {} {}\n", i32: 9, i32: 10);
-    // traceln!("Yeeha {}", i32: 9);
-    // traceln!("C style formatting: %i", i32: 9, .formatter=C_STYLE_FORMAT);
-    // traceln!("No formatting: {} %i")
+    trace!("Yeeha {} {}\n", i32: 9, i32: 10);
+    traceln!("Yeeha {}", i32: 9);
+    traceln!("C style formatting: %i", i32: 9, .formatter=C_STYLE_FORMAT);
+    traceln!("No formatting: {} %i")
 }
