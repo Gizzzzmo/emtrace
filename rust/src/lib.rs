2f05420a3128d2d3be3cfe3a03e390664204b051
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-09-20 23:03:27 +0200

    rust: make byte order id literal as large as possible

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -643,7 +643,8 @@ pub static EMTRACE_MAGIC: [u8; MAGIC_SIZE] = {
 
     let mut idx = 35;
 
-    let byte_order_id_arr = 0x0706050403020100usize.to_ne_bytes();
+    #[allow(overflowing_literals)]
+    let byte_order_id_arr = 0x100f0e0d0c0b0a090807060504030201usize.to_ne_bytes();
     let mut i = 0;
     while i < size_of::<usize>() {
         magic[idx] = byte_order_id_arr[i];
