d5ee1dcd8a6ec5571ff9d6fba4060a59b005a87c
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-12-02 13:18:04 +0100

    fix demo

--- c/examples/demo.c
+++ c/examples/demo.c
@@ -13,11 +13,11 @@ int main(void) {
     for (uint8_t k = 0; k < 1; k++) {
         EMTRACE_F("Hello, World! {:d} {{}}", VAL(int8_t, y));
         EMTRACE("  test {} {}\n");
-        EMTRACE_F("Hello, World! 0x{0:x} {2:d}", VAL(uint8_t, k), VAL(int8_t, 'a'));
+        EMTRACE_F("Hello, World! 0x{0:x} {1:d}", VAL(uint8_t, k), VAL(int8_t, 'a'));
         for (uint8_t i = 5; i < 10; i++) {
             EMTRACE_F("Hello, World! {:d}", VAL(int8_t, y));
             EMTRACE("  test\n");
-            EMTRACELN_F("Hello, World! 0x{0:x} {2:d}", VAL(uint8_t, i), VAL(int8_t, 'a'));
+            EMTRACELN_F("Hello, World! 0x{0:x} {1:d}", VAL(uint8_t, i), VAL(int8_t, 'a'));
             EMTRACELN_F("{:-^20d}", VAL(uint8_t, i));
             for (int j = i; j > 3; j--) {
                 EMTRACELN_F("|{:^18d}|", VAL(int, j));
